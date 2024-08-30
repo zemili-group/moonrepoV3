69a6d2cdc0dfcf48c7a8da14d1683632923820bc
Sean Knowles <s@seanknowles.dev>
2024-08-30 17:10:19 +0100

    chore: fixing deno install

--- apps/zemili/backend/main.ts
+++ apps/zemili/backend/main.ts
@@ -3,13 +3,13 @@ import { createServerService } from "@scope/server";
 export const server = createServerService("zemili-backend");
 
 server.addRoute({
-    method: "get",
-    path: "/",
-    handler: (ctx) => {
-        ctx.response.body = `
+  method: "get",
+  path: "/",
+  handler: (ctx) => {
+    ctx.response.body = `
         🚀🚀🚀 Server is running! 🚀🚀🚀
         `;
-    },
+  },
 });
 
 await server.start(3000);
