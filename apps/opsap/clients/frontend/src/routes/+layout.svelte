e20936a0ae208b56bd94c14eb360268c9c84448a
hcsoylu <hcsoylu.dev@gmail.com>
2024-09-07 02:57:57 +0300

    small changes

---
+++ apps/opsap/clients/frontend/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { User } from 'lucide-svelte';
+	import '../app.css';
+</script>
+
+<div class="container">
+	<div
+		class="flex items-center justify-between bg-gradient-to-r from-blue-700 to-blue-500 px-6 py-4"
+	>
+		<div class="text-white">
+			<img src="/opsap6-white-final.png" alt="logo opsap" class="h-6 w-24" />
+		</div>
+		<Button variant="link" class="space-x-2 text-white"
+			><User size="16px" /> <span>LOGIN </span></Button
+		>
+	</div>
+
+	<slot></slot>
+
+	<div class="flex items-center justify-between bg-black px-10 py-4">
+		<div class="text-white">@ OPSAP</div>
+	</div>
+</div>
--- apps/opsap/clients/frontend/src/routes/+page.svelte
+++ apps/opsap/clients/frontend/src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Card from '$lib/components/ui/card/card.svelte';
+	import { BriefcaseBusiness, Users } from 'lucide-svelte';
+</script>
+
+<main class="flex flex-col">
+	<div class="flex">
+		<div class="flex flex-1 flex-col py-4">
+			<h3 class="text-8xl font-extrabold">Divers</h3>
+			<p class="mt-4 text-2xl">Inshore, Offshore, Salvage</p>
+			<p class="mt-4">
+				Automated Career Tracking,Digital logbooks, <br /> Jobsboard and applicant management
+				platform
+				<br />
+				fro the commercial diving industry
+			</p>
+			<Button class="mt-10 w-fit bg-gradient-to-r from-blue-700 to-blue-500">Signup for free</Button
+			>
+		</div>
+		<div class="flex-1 p-24 pr-10 pt-10">
+			<img src="/divers.svg" alt="logo opsap" class="h-[600px] w-full" />
+		</div>
+	</div>
+
+	<div class="flex justify-between">
+		<Card class="rounded-bl-2 rounded-tr-2 rounded-br-3xl rounded-tl-3xl px-20 py-10">hey hey</Card>
+		<Card class="rounded-bl-2 rounded-tr-2 rounded-br-3xl rounded-tl-3xl px-20 py-10">hey hey</Card>
+		<Card class="rounded-bl-2 rounded-tr-2 rounded-br-3xl rounded-tl-3xl px-20 py-10">hey hey</Card>
+		<Card class="rounded-bl-2 rounded-tr-2 rounded-br-3xl rounded-tl-3xl px-20 py-10">hey hey</Card>
+		<Card class="rounded-bl-2 rounded-tr-2 rounded-br-3xl rounded-tl-3xl px-20 py-10">hey hey</Card>
+	</div>
+	<Card
+		class="rounded-bl-2 rounded-tr-2 mt-2* mt-20 flex justify-between rounded-br-3xl rounded-tl-3xl bg-black px-20 py-10"
+	>
+		<div class="flex flex-col space-y-4 text-white">
+			<Users />
+			<div class="text-4xl">12,900</div>
+			<div>Global Users</div>
+		</div>
+		<div class="flex flex-col space-y-4 text-white">
+			<Users />
+			<div class="text-4xl">12,900</div>
+			<div>Global Users</div>
+		</div>
+		<div class="flex flex-col space-y-4 text-white">
+			<Users />
+			<div class="text-4xl">12,900</div>
+			<div>Global Users</div>
+		</div>
+		<div class="flex flex-col space-y-4 text-white">
+			<Users />
+			<div class="text-4xl">12,900</div>
+			<div>Global Users</div>
+		</div>
+	</Card>
+</main>
