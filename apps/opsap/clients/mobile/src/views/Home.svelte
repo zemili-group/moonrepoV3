0fee5790cb483a02259f6d9a53cd4375a5e7d820
Sean Knowles <s@seanknowles.dev>
2024-09-07 15:37:13 +0100

    chore: deploy mobile to deno deploy

--- apps/opsap/clients/mobile/src/views/Home.svelte
+++ apps/opsap/clients/mobile/src/views/Home.svelte
@@ -22,8 +22,6 @@
 
   // testing change detection
   // testing change detection
-  // testing change detection
-  // testing change detection
 
   const dispatch = createEventDispatcher<{ theme: "ios" | "material" }>();
 </script>
