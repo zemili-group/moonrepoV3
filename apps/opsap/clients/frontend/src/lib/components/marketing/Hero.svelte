07111d26589d5ea368638de8454be7e513706bdc
hcsoylu <hcsoylu.dev@gmail.com>
2024-09-10 23:19:55 +0300

    added grid and card text component to the hero section

--- apps/opsap/clients/frontend/src/lib/components/marketing/Hero.svelte
+++ apps/opsap/clients/frontend/src/lib/components/marketing/Hero.svelte
@@ -32,7 +32,14 @@
 		</SignupForm>
 	</div>
 
-	<div class="hidden flex-1 p-20 pr-0 pt-10 sm:block">
+	<div class="relative hidden flex-1 p-20 pr-0 pt-10 sm:block">
+		<img src="/grid.png" alt="grid" class="absolute top-48 -z-10 h-[240px] -translate-x-4" />
+		<div class="absolute right-12 top-1/3 rounded-xl bg-white px-6 py-3 text-black shadow-lg">
+			Log your work history
+		</div>
+		<div class="absolute left-20 top-2/3 rounded-xl bg-white px-6 py-3 text-black shadow-lg">
+			Build your reputation
+		</div>
 		<img src="/divers.svg" alt="logo opsap" class="h-[600px] w-full" />
 	</div>
 </div>
