b0b3d6c211a783b73e773583f5184c138561fd35
Sean Knowles <s@seanknowles.dev>
2024-10-05 13:45:53 +0100

    chore: setup pre-commit hook

--- apps/opsap/clients/frontend/src/lib/requests/auth-request.ts
+++ apps/opsap/clients/frontend/src/lib/requests/auth-request.ts
@@ -1,66 +1,66 @@
 export async function signup(name: string, email: string, password: string) {
-	console.log("Name:", name)
-	console.log("Email:", email)
-	console.log("Password:", password)
+  console.log("Name:", name)
+  console.log("Email:", email)
+  console.log("Password:", password)
 
-	const url = "http://localhost:3000/signup"
+  const url = "http://localhost:3000/signup"
 
-	try {
-		const response = await fetch(url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				name,
-				email,
-				password,
-				// company,
-				// selectedOptions
-			}),
-		})
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+        // company,
+        // selectedOptions
+      }),
+    })
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`)
-		}
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
 
-		const result = await response.json()
+    const result = await response.json()
 
-		console.log("Server Response:", result)
+    console.log("Server Response:", result)
 
-		return result
-	} catch (error) {
-		console.error("Error:", error)
-		return false
-	}
+    return result
+  } catch (error) {
+    console.error("Error:", error)
+    return false
+  }
 }
 
 export async function login(email: string, password: string) {
-	const url = "http://localhost:3000/login"
+  const url = "http://localhost:3000/login"
 
-	try {
-		const response = await fetch(url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		})
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`)
-		}
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
 
-		const result = await response.json()
+    const result = await response.json()
 
-		console.log("Server Response:", result)
+    console.log("Server Response:", result)
 
-		return result
-	} catch (error) {
-		console.error("Error:", error)
-		return false
-	}
+    return result
+  } catch (error) {
+    console.error("Error:", error)
+    return false
+  }
 }
--- apps/opsap/clients/frontend/src/lib/requests/stats-request.ts
+++ apps/opsap/clients/frontend/src/lib/requests/stats-request.ts
@@ -1,23 +1,23 @@
 export async function getStats(statsId: "users") {
-	const url = `http://localhost:3000/stats/${statsId}`
+  const url = `http://localhost:3000/stats/${statsId}`
 
-	try {
-		const response = await fetch(url, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`)
-		}
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
 
-		const result = await response.json()
+    const result = await response.json()
 
-		return result
-	} catch (error) {
-		console.error("Error:", error)
-		return false
-	}
+    return result
+  } catch (error) {
+    console.error("Error:", error)
+    return false
+  }
 }
